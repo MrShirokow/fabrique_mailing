6694274fe9094a289feb3594e3db7a263957f398
MrShirokow <mr.shirokow@gmail.com>
2022-08-23 22:18:41 +0500

    add client api view (for primary key)

--- application/urls.py
+++ application/urls.py
@@ -1,9 +1,10 @@
-from django.urls import re_path
-from application.views import ClientListAPIView
+from django.urls import re_path, path
+from application.views import ClientListAPIView, ClientAPIView
 
 
 urlpatterns = [
     re_path(r'^clients/?$', ClientListAPIView.as_view()),
+    re_path(r'^clients/(?P<pk>[0-9]*)/?$', ClientAPIView.as_view()),
     # re_path(r'^themes/(?P<theme_id>[0-9]*)/?$', ThemeDetailView.as_view()),
     # re_path(r'^levels/?$', LevelDetailView.as_view()),
     # re_path(r'^categories/?$', CategoryListView.as_view()),
--- application/views.py
+++ application/views.py
@@ -1,11 +1,12 @@
 import json
 
 from django.db import connection
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.http import HttpRequest, JsonResponse, HttpResponseNotFound, HttpResponse
+from django.http import HttpRequest, JsonResponse, HttpResponseNotFound, HttpResponse, Http404
 
 from application.serializers.client_serializer import ClientListSerializer
 from application.entities.client.model import Client
@@ -17,11 +18,21 @@ from application.entities.message.form import MessageForm
 
 
 class ClientListAPIView(APIView):
-    def get(self, request: Request) -> Response:
-        clients = ClientListSerializer(instance=Client.objects.all(), many=True)
-        return Response(clients.data)
+    """
+    Get list of all clients or create new client
+    """
+    def get(self, request: Request, format=None) -> Response:
+        """
+        Get list of clients
+        """
+        clients = Client.objects.all()
+        clients_serializer = ClientListSerializer(instance=clients, many=True)
+        return Response(clients_serializer.data)
 
-    def post(self, request: Request) -> Response:
+    def post(self, request: Request, format=None) -> Response:
+        """
+        Create a new client or 400 (bad request)
+        """
         client_serializer = ClientListSerializer(data=request.data)
         if not client_serializer.is_valid():
             return Response(client_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -31,27 +42,37 @@ class ClientListAPIView(APIView):
 
 
 class ClientAPIView(APIView):
-    def get(self, request: Request) -> Response:
-        clients = ClientListSerializer(instance=Client.objects.all(), many=True)
-        return Response(clients.data)
+    """
+    Get, update or delete a client instance.
+    """
+    def get(self, request: Request, pk: int, format=None) -> Response:
+        """
+        Get client by id or 404
+        """
+        client = get_object_or_404(Client, pk=pk)
+        client_serializer = ClientListSerializer(client)
+        print(connection.queries)
+        return Response(client_serializer.data)
 
-    def post(self, request: Request) -> Response:
-        client_serializer = ClientListSerializer(data=request.data)
+    def put(self, request: Request, pk: int, format=None) -> Response:
+        """
+        Update client by id or 404
+        """
+        client = get_object_or_404(Client, pk=pk)
+        client_serializer = ClientListSerializer(client, data=request.data)
         if not client_serializer.is_valid():
             return Response(client_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         client_serializer.save()
-        return Response('client was created successfully', status=status.HTTP_201_CREATED)
+        return Response('client was updated successfully', status=status.HTTP_200_OK)
 
-    def put(self, request: Request) -> Response:
-        client_serializer = ClientListSerializer(data=request.data)
-        if not client_serializer.is_valid():
-            return Response(client_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        return Response('client was updated successfully', status=200)
-
-    def delete(self, request: Request) -> Response:
-        return Response('client was deleted successfully', status=200)
+    def delete(self, request: Request, pk: int, format=None) -> Response:
+        """
+        Delete client by id or 404
+        """
+        client = get_object_or_404(Client, pk=pk)
+        client.delete()
+        return Response('client was deleted successfully', status=status.HTTP_204_NO_CONTENT)
 
 
 class NotificationAPIView(APIView):

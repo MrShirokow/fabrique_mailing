d1f79ae1501fa857b60922fd1dcfb6f85521805f
MrShirokow <mr.shirokow@gmail.com>
2022-09-10 15:39:53 +0500

    update tests, add error cases for models

--- mailing_service/tests/test_models.py
+++ mailing_service/tests/test_models.py
@@ -1,6 +1,6 @@
 import pytest
 
-from django.db import DataError
+from django.db import DataError, IntegrityError
 from django.core.exceptions import ValidationError
 
 from mailing_service.models.client import Client
@@ -9,7 +9,10 @@ from mailing_service.models.notification import Notification
 
 
 @pytest.mark.django_db
-def test_client_model():
+def test_client_model_ok():
+    """
+    Testing successful creating of  client model
+    """
     client = Client.objects.create(
         phone_number='79007886151',
         tag='tag_1',
@@ -25,11 +28,26 @@ def test_client_model():
 
 
 @pytest.mark.django_db
+def test_client_unique_phone_number():
+    """
+    Testing unique of phone number
+    """
+    Client.objects.create(phone_number='79007886151', tag='tag_1',
+                          mobile_operator_code='900', time_zone='Asia/Yekaterinburg')
+    with pytest.raises(IntegrityError):
+        Client.objects.create(phone_number='79007886151', tag='tag_2',
+                              mobile_operator_code='900', time_zone='Europe/Moscow')
+
+
+@pytest.mark.django_db
 @pytest.mark.parametrize('error, phone_number, tag, mobile_operator_code, time_zone', [
     (ValidationError, '79001005070', 'tag_1', '900', 'Europe/Unknown'),
     (DataError, '79001005070', 'tag_1', '9111', 'Europe/Moscow'),
     (DataError, '7900100507080', 'tag_1', '900', 'Europe/Moscow')])
 def test_client_model_with_error(error, phone_number, tag, mobile_operator_code, time_zone):
+    """
+    Testing creating client model with error
+    """
     with pytest.raises(error):
         Client.objects.create(
             phone_number=phone_number,
@@ -41,6 +59,9 @@ def test_client_model_with_error(error, phone_number, tag, mobile_operator_code,
 
 @pytest.mark.django_db
 def test_notification_model():
+    """
+    Testing successful creating of notification model
+    """
     notification = Notification.objects.create(
         start_datetime='2022-09-06 10:00:00',
         end_datetime='2022-09-10 23:59:00',
@@ -56,7 +77,29 @@ def test_notification_model():
 
 
 @pytest.mark.django_db
+@pytest.mark.parametrize('error, start_datetime, end_datetime, text, mailing_filter', [
+    (ValidationError, '2022-09-32 10:00:00', '2022-09-10 10:00:00', 'Some text', {'mobile_operator_code': 900}),
+    (ValidationError, '2022-09-10 10:00:00', '2022-09-32 10:00:00', 'Some text', {'tag': 'tag_1'}),
+    (IntegrityError, '2022-09-10 10:00:00', '2022-09-11 10:00:00', 'Some text', None),
+    (IntegrityError, '2022-09-10 10:00:00', '2022-09-11 10:00:00', None, {'tag': 'tag_1'})])
+def test_notification_model_with_error(error, start_datetime, end_datetime, text, mailing_filter):
+    """
+    Testing creating notification model with error
+    """
+    with pytest.raises(error):
+        Notification.objects.create(
+            start_datetime=start_datetime,
+            end_datetime=end_datetime,
+            text=text,
+            mailing_filter=mailing_filter,
+        )
+
+
+@pytest.mark.django_db
 def test_message_model():
+    """
+    Testing successful creating of message model
+    """
     notification = Notification.objects.create(start_datetime='2022-09-06 10:00:00',
                                                end_datetime='2022-09-10 23:59:00',
                                                text='Attention! Notification text!',

bbd6912b0696f788978e6cc9fc839aeff9c365ac
MrShirokow <mr.shirokow@gmail.com>
2022-09-14 16:19:45 +0500

    add validation to client and notification serializers

--- mailing_service/serializers/client.py
+++ mailing_service/serializers/client.py
@@ -5,6 +5,12 @@ from rest_framework import serializers
 from mailing_service.models.client import Client
 
 
+def get_default(attr: str, instance):
+    if instance is None:
+        return None
+    return getattr(instance, attr)
+
+
 class ClientSerializer(serializers.ModelSerializer):
     """
     Serializer for client entity
@@ -14,3 +20,11 @@ class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
         fields = ('id', 'phone_number', 'tag', 'mobile_operator_code', 'time_zone')
+
+    def validate(self, data):
+        phone_number = data.get('phone_number', get_default('phone_number', self.instance))
+        mobile_operator_code = data.get('mobile_operator_code',
+                                        get_default('mobile_operator_code', self.instance))
+        if phone_number[1:4] != str(mobile_operator_code):
+            raise serializers.ValidationError({'mobile_operator_code': 'invalid value of mobile operator code'})
+        return data

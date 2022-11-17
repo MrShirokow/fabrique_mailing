40c2ff9cfb6a9918713362d8fb2f3e80fc3c6d39
MrShirokow <mr.shirokow@gmail.com>
2022-11-18 01:00:32 +0500

    fix celery tasks in tests

--- mailing_service/tests/test_celery_tasks.py
+++ mailing_service/tests/test_celery_tasks.py
@@ -1,52 +1,27 @@
 import pytest
 import datetime
 
+from unittest import mock
+
 from mailing_service import tasks
 from mailing_service.models.message import Message
-from mailing_service.models.client import Client
-from mailing_service.models.notification import Notification
 from mailing_service.models.success_client import SuccessClient
+from mailing_service.tasks import create_model_entries
 
 
-@pytest.fixture
-def mailing_test_data():
-    start_datetime = (datetime.date.today() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
-    end_datetime = (datetime.date.today() + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
-    notification_data = [Notification(**{'start_datetime': f'{start_datetime} 10:00:00',
-                                         'end_datetime': f'{end_datetime} 23:59:59',
-                                         'text': 'First Notification',
-                                         'mailing_filter': {'tag': 'tag_1'}}),
-                         Notification(**{'start_datetime': f'{start_datetime} 10:00:00',
-                                         'end_datetime': f'{end_datetime} 23:59:59',
-                                         'text': 'Second Notification',
-                                         'mailing_filter': {'tag': 'tag_1', 'mobile_operator_code': '922'}}),
-                         Notification(**{'start_datetime': '2020-10-10 10:00:00',
-                                         'end_datetime': '2020-10-20 23:59:59',
-                                         'text': 'It was sent',
-                                         'mailing_filter': {'mobile_operator_code': '900'}})]
-    client_data = [Client(**{'phone_number': '79227886151', 'tag': 'tag_1',
-                             'mobile_operator_code': '922', 'time_zone': 'Europe/Moscow'}),
-                   Client(**{'phone_number': '79220009912', 'tag': 'tag_2',
-                             'mobile_operator_code': '922', 'time_zone': 'Asia/Omsk'}),
-                   Client(**{'phone_number': '79001459134', 'tag': 'tag_1',
-                             'mobile_operator_code': '900', 'time_zone': 'Asia/Omsk'})]
-    Client.objects.bulk_create(client_data)
-    Notification.objects.bulk_create(notification_data)
-    return {'notification_data': notification_data, 'client_data': client_data}
-
-
-@pytest.mark.django_db
 def test_mailing_get_notification(mailing_test_data):
     notifications = tasks.get_notifications()
-    expected = [{
-        'id': notification.id, 
-        'text': notification.text, 
-        'mailing_filter': notification.mailing_filter
-    } for notification in mailing_test_data['notification_data'][:2]]
+    expected = [
+        {
+            'id': notification.id, 
+            'text': notification.text, 
+            'mailing_filter': notification.mailing_filter
+        } 
+        for notification in mailing_test_data['notification_data'][:2]
+    ]
     assert list(notifications) == expected
 
 
-@pytest.mark.django_db
 def test_mailing_get_clients(mailing_test_data):
     notification = mailing_test_data['notification_data'][1]
     clients = tasks.get_clients(notification.mailing_filter, notification.id)
@@ -54,18 +29,31 @@ def test_mailing_get_clients(mailing_test_data):
     assert list(clients) == [(expected_client.id, expected_client.phone_number)]
 
 
-@pytest.mark.django_db
+@pytest.mark.celery
+def test_mailing_send_message(celery_worker):
+    status_ok, _ = tasks.send_message.delay({
+        'id': 1, 'phone': 79635709980, 'text': 'Some Text'
+    }).get()
+    status_bad, _ = tasks.send_message.delay({
+        'id': 2, 'phone': 79635709980, 'text': 'Some Text'
+    }).get()
+    assert status_ok == 200
+    assert status_bad == 400
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.celery
 @pytest.mark.parametrize('model_name', ['Message', 'SuccessClient'])
-def test_mailing_create_model_entries(mailing_test_data, model_name):
+def test_mailing_create_model_entries(mailing_test_data, celery_worker, model_name):
     message_data = [
         {
-            'notification': mailing_test_data['notification_data'][0],
-            'client': mailing_test_data['client_data'][1],
+            'notification_id': mailing_test_data['notification_data'][0].id,
+            'client_id': mailing_test_data['client_data'][1].id,
             'is_sending': True
         },
         {
-            'notification': mailing_test_data['notification_data'][1],
-            'client': mailing_test_data['client_data'][0],
+            'notification_id': mailing_test_data['notification_data'][1].id,
+            'client_id': mailing_test_data['client_data'][0].id,
             'is_sending': True
         }
     ]
@@ -82,19 +70,17 @@ def test_mailing_create_model_entries(mailing_test_data, model_name):
     data = {'Message': message_data, 'SuccessClient': success_clients_data}
     models = {'Message': Message, 'SuccessClient': SuccessClient}
     assert models[model_name].objects.count() == 0
-    tasks.create_model_entries(model_name, data[model_name])
+    task_result = tasks.create_model_entries.delay(model_name, data[model_name])
+    _ = task_result.get()
+    assert task_result.status == 'SUCCESS'
     assert models[model_name].objects.count() == 2
 
 
-def test_mailing_send_message():
-    status_ok, time = tasks.send_message(data={'id': 1, 'phone': 79635709980, 'text': 'Some Text'})
-    status_bad, time = tasks.send_message(data={'id': 2, 'phone': 79635709980, 'text': 'Some Text'})
-    assert status_ok == 200
-    assert status_bad == 400
-
-
-# @pytest.mark.django_db
-# def test_run_mailing(mailing_test_data):
-#     assert Message.objects.count() == 0
-#     tasks.run_mailing()
-#     assert Message.objects.count() == 3
+@mock.patch('celery.result.AsyncResult.get', lambda data: (200, datetime.datetime.now()))
+@mock.patch('mailing_service.tasks.create_model_entries', create_model_entries)
+def test_run_mailing(mailing_test_data):
+    assert Message.objects.count() == 0
+    assert SuccessClient.objects.count() == 0
+    tasks.run_mailing()
+    assert SuccessClient.objects.count() == 3
+    assert Message.objects.count() == 3
